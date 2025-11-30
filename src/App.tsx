153929c005e35b77e0749ab4359453a46012ba20
TCOD <amirprogrammer38@gmail.com>
2025-11-30 10:55:40 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl text-purple-500 font-bold">Dragon App</h1>
+    </div>
+  );
+}
+
+export default App;
